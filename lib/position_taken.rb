060c9006b44485046a3f49c540e2d05963c5929b
Joshua Needham <joshuabneedham@gmail.com>
2017-10-01 01:17:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  array[index_number] != ""
-
-
+  if array[index_number] != ""
+    puts "Please choose a number."
+  end
 end
