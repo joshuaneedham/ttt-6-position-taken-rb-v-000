4d2bf610a811a984cad7d000543a0c20f723085d
Joshua Needham <joshuabneedham@gmail.com>
2017-10-01 01:37:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   #Check for an empty board is equal to " "
-  board[index] == " "
+  board[index] == " " || board[index] == ""
 end
